f43cc58918b72f36cc2ec3120f5d30386101b7b5
sinergia-12 <erickgallardo89@yahoo.com>
2019-08-19 10:20:38 -0600

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sinergia-12\PycharmProjects\reproductivaApp\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
